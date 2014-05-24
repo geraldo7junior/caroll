4454061f8d106b8ce126bb088fc32ed7c923649d
jrjcsj@gmail.com <jrjcsj@gmail.com@a001e179-2d34-b645-6d1c-8dddaa59dabd>
2014-05-24 18:55:14 +0000

    Criação de Value Object para usuario

---
+++ trunk/src/br/com/caroll/vo/UsuarioVO.java
@@ -0,0 +1,36 @@
+package br.com.caroll.vo;
+
+public class UsuarioVO {
+	
+    private String nomeUsuario;
+    private String loginUsuario;
+    private String sexoUsuario;
+    private String dataNascimentoUsuario;
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getLoginUsuario() {
+		return loginUsuario;
+	}
+	public void setLoginUsuario(String loginUsuario) {
+		this.loginUsuario = loginUsuario;
+	}
+	public String getSexoUsuario() {
+		return sexoUsuario;
+	}
+	public void setSexoUsuario(String sexoUsuario) {
+		this.sexoUsuario = sexoUsuario;
+	}
+	public String getDataNascimentoUsuario() {
+		return dataNascimentoUsuario;
+	}
+	public void setDataNascimentoUsuario(String dataNascimentoUsuario) {
+		this.dataNascimentoUsuario = dataNascimentoUsuario;
+	}
+    
+    
+
+}
