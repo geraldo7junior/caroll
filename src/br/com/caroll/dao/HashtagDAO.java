953da1d20f3e684a4a1e2237b3a292cd4062c1cf
jrjcsj@gmail.com <jrjcsj@gmail.com@a001e179-2d34-b645-6d1c-8dddaa59dabd>
2014-05-25 21:11:26 +0000

    Modificações nas classes HashtagDAO, TweetDAO, UsuarioDAO

--- src/br/com/caroll/dao/HashtagDAO.java
+++ src/br/com/caroll/dao/HashtagDAO.java
@@ -2,10 +2,14 @@ package br.com.caroll.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.caroll.conexao.ConnectionFactory;
 import br.com.caroll.vo.HashtagVO;
+import br.com.caroll.vo.TweetVO;
 
 public class HashtagDAO {
 	
@@ -17,15 +21,13 @@ Connection con;
 	
 	public void inserir (HashtagVO hashTagVO) {
 		
-		String sql = "INSERT Hashtag(idHashtag, "
-				+ "dadoHashtag) VALUES (?, ?)";
+		String sql = "INSERT Hashtag (dadoHashtag) VALUES (?)";
 		
 		try {
 			
 			PreparedStatement stmt = con.prepareStatement(sql);
 			
-			stmt.setInt(1, hashTagVO.getIdHashtag());
-			stmt.setString(2, hashTagVO.getDadoHashtag());
+			stmt.setString(1, hashTagVO.getDadoHashtag());
 
 			stmt.execute();
 			
@@ -37,5 +39,33 @@ Connection con;
 		}
 	}
 
+public List <HashtagVO> listarHashTag (HashtagVO hashtagVO) {
+		
+		List <HashtagVO> listaHashtag = new ArrayList<HashtagVO>();
+		String sql = "SELECT * FROM Hashtag WHERE dadoHashtag = ?";
+		
+		try {
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			stmt.setString(1, hashtagVO.getDadoHashtag());
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			
+			while (rs.next()) {
+				hashtagVO.setIdHashtag(rs.getInt("idHashtag"));
+				hashtagVO.setDadoHashtag(rs.getString("dadoHashtag"));
+				
+				listaHashtag.add(hashtagVO);
+	
+			}
+			
+			return listaHashtag;
+		}catch (SQLException ex) {
+			throw new RuntimeException(ex);
+			
+		}
+	}
 
 }
