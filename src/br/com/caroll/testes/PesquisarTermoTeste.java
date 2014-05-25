aa13eaccd21d75f040e6366931765599a93ea89a
jrjcsj@gmail.com <jrjcsj@gmail.com@a001e179-2d34-b645-6d1c-8dddaa59dabd>
2014-05-25 21:12:02 +0000

    Modificações na classe de teste PesquisaTermo

--- src/br/com/caroll/testes/PesquisarTermoTeste.java
+++ src/br/com/caroll/testes/PesquisarTermoTeste.java
@@ -1,11 +1,13 @@
 package br.com.caroll.testes;
 
+import java.sql.SQLException;
+
 import twitter4j.TwitterException;
 import br.com.caroll.utilitarios.PesquisarTermo;
 
 public class PesquisarTermoTeste {
 
-	public static void main(String[] args) throws TwitterException {
+	public static void main(String[] args) throws TwitterException, ClassNotFoundException, SQLException {
 		
 		PesquisarTermo pesquisarTermo = new PesquisarTermo();
 		
