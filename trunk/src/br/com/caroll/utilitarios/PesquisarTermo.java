d1073872d5d78de8491a3c2d61d30496447efa2e
jrjcsj@gmail.com <jrjcsj@gmail.com@a001e179-2d34-b645-6d1c-8dddaa59dabd>
2014-05-25 21:12:34 +0000

    Modificações na classe Utilitária PesquisaTermo

--- trunk/src/br/com/caroll/utilitarios/PesquisarTermo.java
+++ trunk/src/br/com/caroll/utilitarios/PesquisarTermo.java
@@ -1,5 +1,17 @@
 package br.com.caroll.utilitarios;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caroll.dao.HashtagDAO;
+import br.com.caroll.dao.HashtagTweetDAO;
+import br.com.caroll.dao.TweetDAO;
+import br.com.caroll.dao.UsuarioDAO;
+import br.com.caroll.vo.HashtagTweetVO;
+import br.com.caroll.vo.HashtagVO;
+import br.com.caroll.vo.TweetVO;
+import br.com.caroll.vo.UsuarioVO;
 import twitter4j.HashtagEntity;
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -11,7 +23,7 @@ import twitter4j.URLEntity;
 
 public class PesquisarTermo {
 
-	 public void pesquisarTermo (String termo) throws TwitterException {	
+	 public void pesquisarTermo (String termo) throws TwitterException, ClassNotFoundException, SQLException {	
 		 // The factory instance is re-useable and thread safe.
 			 Twitter twitter = TwitterFactory.getSingleton();
 			    Query query = new Query(termo);
@@ -22,25 +34,111 @@ public class PesquisarTermo {
 			    
 			    for (Status status : result.getTweets()) {
 			        //System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-			    	 System.out.println("nome do usuário: "+status.getUser().getName());
-				        System.out.println("texto do tweet: "+status.getText());
-				        System.out.println("Data do tweet: "+status.getCreatedAt());
-				        hashtags = status.getHashtagEntities();
-				        System.out.println("Hash tags do tweet: ");
-				        for (int i = 0; i < hashtags.length; i++) {
-							System.out.println("hashtags do tweet: "+hashtags[i].getText());
-						}
-				        System.out.println("Tweet favoritado: "+status.isFavorited());
-				        System.out.println("geolocalização ativada:"+status.getUser().isGeoEnabled());
-				        System.out.println("Local: "+status.getUser().getLocation());
-				        System.out.println("Time zone: "+status.getUser().getTimeZone());
-				        System.out.println("Informações do usuário: "+status.getUser().getDescription());
-				        System.out.println("Possivelmente sensível: "+status.isPossiblySensitive());
-				        
-				      /*  for (int i = 0; i < urlEntity.length; i++) {
-				        	System.out.println("entidade url: "+urlEntity[i].getText());
-				        }*/
-				        System.out.println("Idioma : "+status.getLang()+"\n");
+			        System.out.println("nome do usuário: "+status.getUser().getName());
+			        System.out.println("texto do tweet: "+status.getText());
+			        System.out.println("Data do tweet: "+status.getCreatedAt());
+			        hashtags = status.getHashtagEntities();
+			       /* for (int i = 0; i < hashtags.length; i++) {
+						System.out.println("hashtags do tweet "+i+": "+hashtags[i].getText());
+					}*/
+			        System.out.println("Tweet favoritado: "+status.isFavorited());
+			        System.out.println("geolocalização ativada:"+status.getUser().isGeoEnabled());
+			        System.out.println("Local: "+status.getUser().getLocation());
+			        System.out.println("Time zone: "+status.getUser().getTimeZone());
+			        System.out.println("Quantidade de retweets: "+status.getRetweetCount());
+			        System.out.println("Informações do usuário: "+status.getUser().getDescription());
+			        //System.out.println("Lugar: "+status.getPlace().getGeometryCoordinates().toString());
+			        
+			        System.out.println("usuário criado em: "+status.getUser().getCreatedAt());
+			        System.out.println("Possivelmente sensível: "+status.isPossiblySensitive());
+			        System.out.println("Link no perfil: "+status.getSource());
+			        System.out.println("ScreenName: "+status.getUser().getScreenName());
+			        System.out.println("Status: "+status.getUser().getStatus());
+			        System.out.println("Idioma : "+status.getLang());
+				       
+			        UsuarioVO usuarioVO = new UsuarioVO();
+			        UsuarioDAO usuarioDAO = new UsuarioDAO();
+					TweetVO tweetVO = new TweetVO();
+					TweetDAO tweetDAO = new TweetDAO();
+					
+					HashtagTweetVO hashtagTweetVO = new HashtagTweetVO();
+					HashtagTweetDAO hashtagTweetDAO = new HashtagTweetDAO();
+					
+					HashtagVO hashTagVO = new HashtagVO();
+					HashtagDAO hashtagDAO = new HashtagDAO();
+					 
+					usuarioVO.setNomeUsuario(status.getUser().getName().toString());
+					usuarioVO.setLoginUsuario(status.getUser().getScreenName());
+					
+					usuarioDAO.inserir(usuarioVO);
+					
+					System.out.println("Usuario inserido");
+					
+					
+					List <UsuarioVO> listaUsuario =  usuarioDAO.listarUsuario(usuarioVO);
+					
+					
+					if (listaUsuario.isEmpty()) {
+						System.out.println("A lista de usuários está vazia");
+					}else
+						System.out.println("A lista de usuários não está vazia");
+					tweetVO.setPostTweet(status.getText().toString());
+					
+					String dia = String.valueOf(status.getCreatedAt().getDate());
+					String mes = String.valueOf(status.getCreatedAt().getMonth()+1);
+					String ano = String.valueOf((status.getCreatedAt().getYear() + 1900));
+					String dataTweet = dia+"/"+mes+"/"+ano;
+				
+					tweetVO.setDataTweet(dataTweet);
+		
+					tweetVO.setUsuarioFk(listaUsuario.get(0).getIdUsuario());
+					
+					
+					tweetDAO.inserir(tweetVO);
+					System.out.println("Tweet inserido com sucesso");
+					
+			    	if (hashtags.length != 0) {
+			    		
+			    		System.out.println("Esse tweet possui hashtags!");
+			    		for (int i = 0; i < hashtags.length; i++) {
+			    			
+			    			hashTagVO.setDadoHashtag(hashtags[i].getText());
+			    			hashtagDAO.inserir(hashTagVO);
+			    			System.out.println("Hashtag inserida com sucesso");
+			    			
+			    			List <HashtagVO> listaHashtag = hashtagDAO.listarHashTag(hashTagVO);
+			    			List <TweetVO> listaTweet = tweetDAO.listarTweet(tweetVO);
+			    			
+			    			if (listaHashtag.isEmpty()) {
+			    				System.out.println("Lista hashtag está vazia");
+			    			}else
+			    				System.out.println("Lista hashtag não está vazia");
+			    			
+			    			if (listaTweet.isEmpty()) {
+			    				System.out.println("Lista tweet está vazia");
+			    			}else
+			    				System.out.println("Lista tweet não está vazia");
+			    			
+			    			
+			    			
+			    			hashtagTweetVO.setHashtagFk(listaHashtag.get(0).getIdHashtag());
+			    			hashtagTweetVO.setTweetFk(listaTweet.get(0).getIdTweet());
+			    			
+			    			hashtagTweetDAO.inserir(hashtagTweetVO);
+			    			System.out.println("HashtagTweet inserida com sucesso");
+			    			
+			    		}
+			    
+			    	}else
+			    		
+			    		System.out.println("Esse tweet não possui hashtags");
+			    	
+			    	System.out.println("Informações pesquisadas e inseridas no banco com sucesso \n");
+			    	
+					
 			    }
+			    
+			    
+			
 		}
 }
